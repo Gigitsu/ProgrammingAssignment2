2d3115d5f694219d3b6de00e97597256ea587864
Gigitsu <gigitsu.23@gmail.com>
2014-12-17 15:49:00 +0100

    First attempt

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    im <- NULL
+    set <- function(y) {
+        x <<- y
+        im <<-NULL
+    }
+    get <-function() x
 
+    setInvertedMatrix <- function(invertedMatrix) im <<- invertedMatrix
+    getInvertedMatrix <- function() im
+
+    isCached <- function() !is.null(im)
+
+    list(set = set, get = get,
+         isCached = isCached,
+         setInvertedMatrix = setInvertedMatrix,
+         getInvertedMatrix = getInvertedMatrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    if(!(x$isCached())) {
+        message("filling the cache")
+        x$setInvertedMatrix(solve(x, ...))
+    }
+
+    x$getInvertedMatrix()
 }
